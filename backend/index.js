ca2470e72c02f7e7982d4c48927f9e86a54d11ea
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-04-10 23:55:04 +0530

    db api for Pins

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-
+const pinRoute = require("./routes/pins");
 dotenv.config();
  
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
     })
@@ -12,6 +14,8 @@ mongoose.connect(process.env.MONGO_URL, {
         console.log("MongoDB Connected");
     })
     .catch((e) => console.log(e));
+
+    app.use("/api/pins", pinRoute);
 
 app.listen(8080,()=>{
     console.log("Backend Running")
