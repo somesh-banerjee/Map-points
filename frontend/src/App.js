7ba46c089b511edcaf498867e5307e8c2e4e6cb9
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-04-13 16:43:08 +0530

    Added Mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import Map,{ Marker } from 'react-map-gl';
+
 function App() {
+  const [viewState, setViewState] = useState({
+    latitude: 22.572645,
+    longitude: 88.363892,
+    zoom: 8
+  });
   return (
     <div className="App">
-      
+      <Map
+      {...viewState}
+      onMove={evt => setViewState(evt.viewState)}
+      style={{width: 800, height: 600}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+    >
+    </Map>
     </div>
   );
 }
