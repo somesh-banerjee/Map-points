1b0def481495569f9466e1add6d8c650def9444d
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-04-21 05:31:48 +0530

    build changes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const path = require('path')
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 dotenv.config();
@@ -10,14 +11,22 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true
-    })
-    .then(()=>{
-        console.log("MongoDB Connected");
-    })
-    .catch((e) => console.log(e));
+})
+.then(()=>{
+    console.log("MongoDB Connected");
+})
+.catch((e) => console.log(e));
 
-    app.use("/api/users", userRoute);
-    app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
 
 app.listen(8080,()=>{
     console.log("Backend Running")
