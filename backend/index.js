e972862ad52e1027f85f3cc05e10da189742dbf3
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-04-21 14:17:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 dotenv.config();
  
+const __myPath = path.resolve();
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
@@ -21,11 +23,13 @@ app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+//app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__myPath, "/frontend/build")));
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  //res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  res.sendFile(path.resolve(__myPath, "frontend", "build", "index.html"))
 })
 
 app.listen(8080,()=>{
